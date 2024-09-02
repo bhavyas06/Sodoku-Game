d8aa484586293781439db79711fad9ebb0713323
bhavyas06 <122335498+bhavyas06@users.noreply.github.com>
2024-09-02 22:04:51 +0530

    SudokuStruct

---
+++ Sodoku.h
@@ -0,0 +1,14 @@
+#ifndef _Sodoku_H_
+#define _Soduku_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int **grid;
+    int N;
+    int M;
+    int diff;
+} Sudoku;
+
+#endif /*_Sudoku_H_*/
