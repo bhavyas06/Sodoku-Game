c50907efdfb46814c5f9235452a01d72bddce996
bhavyas06 <122335498+bhavyas06@users.noreply.github.com>
2024-09-03 17:29:12 +0530

    Fill The Grid

---
+++ Filling.c
@@ -0,0 +1,62 @@
+#include "Filling.h"
+
+void fillValues(Sudoku *sudoku) {
+    fillDiagnols(sudoku);
+    fillRemaining(sudoku, 0, sudoku->M);
+    removeDigits(sudoku);
+}
+
+void fillDiagnols(Sudoku *sudoku) {
+    for(int i=0; i<sudoku->N; i = i+sudoku->M)
+        fillBox(sudoku, i, i);
+}
+
+void fillBox(Sudoku *sudoku, int row, int col) {
+    int num;
+    for(int i=0; i<sudoku->M; i++) {
+        for(int j=0; j<sudoku->M; j++) {
+            do {
+                num = randomGenerator(sudoku->N);
+            } while(!unUsedInBox(sudoku, row, col, num));
+            sudoku->grid[row+i][col+j] = num;
+        }
+    }
+}
+
+int fillRemaining(Sudoku* sudoku, int i, int j) {
+    if (j >= sudoku->N && i < sudoku->N - 1) {
+        i = i + 1;
+        j = 0;
+    }
+
+    if (i >= sudoku->N && j >= sudoku->N)
+        return 1;
+
+    if (i < sudoku->M) {
+        if (j < sudoku->M)
+            j = sudoku->M;
+    } 
+    else if (i < sudoku->N - sudoku->M) {
+        if (j == (int)(i / sudoku->M) * sudoku->M)
+            j = j + sudoku->M;
+    } 
+    else {
+        if (j == sudoku->N - sudoku->M) {
+            i = i + 1;
+            j = 0;
+            if (i >= sudoku->N)
+                return 1;
+        }
+    }
+
+    for (int num = 1; num <= sudoku->N; num++) {
+        if (CheckIfSafe(sudoku, i, j, num)) {
+            sudoku->grid[i][j] = num;
+            if (fillRemaining(sudoku, i, j + 1))
+                return 1;
+            sudoku->grid[i][j] = 0;
+        }
+    }
+    
+    return 0;
+}
---
+++ Filling.h
@@ -0,0 +1,6 @@
+#include "IsSafe.h"
+
+void fillValues(Sudoku *sodoku);
+void fillDiagnols(Sudoku *sodoku);
+void fillBox(Sudoku *sodoku, int row, int col);
+int fillRemaining(Sudoku *sudoku, int i, int j);
