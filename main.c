2e89cd04e52a0dd7928a7cc54f23978357eb4a81
bhavyas06 <122335498+bhavyas06@users.noreply.github.com>
2024-09-05 00:07:45 +0530

    Creating menu

--- main.c
+++ main.c
@@ -8,7 +8,9 @@ int main() {
     int diff = 0;
     
     int diff_mode;
-    printf("Welcome to Sudoku!!\n");
+    printf("Welcome to Sudoku!!\n\n");
+    printf("Instructions:\nInput the row number, column number and the element you want to fill the particular cell with.\nYou can type '-1' to escape where you will be given the choice to either quit the game or see the solution.\n");
+
     printf("\nModes\n1. Easy\n2. Medium\n3. Hard\n");
 
     bool flag = true;
@@ -23,10 +25,13 @@ int main() {
         }
     }
     
-
     sudoku = createSudoku(N, diff);
     fillValues(sudoku);
     printGrid(sudoku);
+
+    int lives = 5;
+    int op;
+
 
     for (int i = 0; i < N; i++) {
         free(sudoku->grid[i]);
