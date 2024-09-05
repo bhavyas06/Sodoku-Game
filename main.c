07b47ee201f56b2cd282a4f02808e8ee02925da2
bhavyas06 <122335498+bhavyas06@users.noreply.github.com>
2024-09-05 13:12:28 +0530

    Upated the menu to take input from user

--- main.c
+++ main.c
@@ -33,6 +33,59 @@ int main() {
     int lives = 5;
     int op;
 
+    while (lives != 0 && diff != 0) {
+        int row, col, num;
+        printf("\nEnter row number: ");
+        scanf("%d", &row);
+
+        if(row==-1) {
+            escapeMenu(sudoku);
+            break;
+        }
+
+        printf("Enter column number: ");
+        scanf("%d", &col);
+
+        if(col==-1) {
+            escapeMenu(sudoku);
+            break;
+        }
+
+        printf("Enter number: ");
+        scanf("%d", &num);
+        printf("\n");
+
+        if(checkBounds(row, col, num) == false) {
+            printf("\nInput Not Valid. Try Again.\n\n");
+            printGrid(sudoku);
+        }
+        else {
+            if(sudoku->grid[row][col]!=0) {
+                printf("Cell not Empty. Try Again.\n\n");
+                // system("clear");
+                printGrid(sudoku);
+            }
+
+            else {
+                if (SolveSudoku(sudoku, row, col, num)) {
+                    printGrid(sudoku);
+                    diff--;
+                    printf("\nLives Remaining: %d", lives);
+                    printf("\nCells Remaining: %d\n", diff);
+                }
+
+                else {
+                    printf("\nWrong Input. Try Again\n");
+                    lives--;
+                    printf("Lives Left: %d\n", lives);
+                    printf("Cells Remaining: %d\n", diff);
+                    printGrid(sudoku);
+                }
+            }
+        }
+        
+    }
+
 
     for (int i = 0; i < N; i++) {
         free(sudoku->grid[i]);
